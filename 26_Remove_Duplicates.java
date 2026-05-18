9269741152aff28bdf4d8586a8f87233913a9cef
Nitesh <niteshgupta919843@gmail.com>
2026-05-18 10:04:17 +0530

    26LC Solution

---
+++ 26_Remove_Duplicates.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+      int i = 0;
+      for(int j = 1; j < nums.length; j++){
+        if(nums[i] != nums[j] ){
+            i++;
+        nums[i] = nums[j];
+      }
+       
+    }
+    return  i +1;  
+}
+public static  void main(String [] args){
+    int [] nums = {1 , 1  , 2 ,3 , 3 };
+    int k = new Solution().removeDuplicates(nums);
+
+    for(int i  = 0; i < k; i++){
+        System.out.print(nums[i] + " ");
+    }
+}
+}
